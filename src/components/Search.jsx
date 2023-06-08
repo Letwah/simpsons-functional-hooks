e574fdacd41bffc16980886db123a7ea8c7f178b
Colette <hello@colettesmith.com>
2023-06-08 13:44:28 +0100

    put back like dislike sort

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,44 +1,18 @@
-// const Search = (props) => {
-//   const { onSearchInput, onLikeDislikeInput, onCharacterInput } = props;
-
 import { useState } from "react";
 import { validate } from "../validation";
 
-//   return (
-//     <>
-//       <input
-//         id="characterInput"
-//         onInput={onCharacterInput}
-//         type="text"
-//         placeholder="start typing ..."
-//       ></input>
-//       <input
-//         onInput={onSearchInput}
-//         type="text"
-//         placeholder="Search Characters ..."
-//       />
-//       {/* {" "} */}
-
-//       <select onChange={onLikeDislikeInput}>
-//         <option value="">Reset</option>
-//         <option value="liked">Liked</option>
-//         <option value="notLiked">Not Liked</option>
-//       </select>
-//     </>
-//   );
-// };
-
 const Search = (props) => {
-  const { onCharacterInput, characterInput } = props;
+  const { onCharacterInput, characterInput, onLikeDislikeInput } = props;
   const [errors, setErrors] = useState(null); //null mean no errors
 
   const onInput = async (e) => {
     onCharacterInput(e.target.value);
 
-    //validate input
+    //validate input - can write like this
 
     // const res = await validate(characterInput);
     // setErrors(res);
+
     //OR
 
     setErrors(
@@ -56,6 +30,12 @@ const Search = (props) => {
         placeholder="start typing ..."
       />
       <p>{errors && errors.characterId}</p>
+
+      <select onChange={onLikeDislikeInput}>
+        <option value="">Reset</option>
+        <option value="liked">Liked</option>
+        <option value="notLiked">Not Liked</option>
+      </select>
     </>
   );
 };
